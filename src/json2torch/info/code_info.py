8bd4aeaccbb85a5da143077ec24108cd43469c91
Zero-nnkn <nnkhoinguyen9@gmail.com>
2023-01-11 00:06:43 +0700

    chore: add attribute abstract class

--- src/json2torch/info/code_info.py
+++ src/json2torch/info/code_info.py
@@ -1,12 +1,17 @@
 from abc import ABC, abstractmethod
 
 
-class CodeInfo(ABC):
-    @abstractmethod
-    def generate_code(cls, single_node):
-        pass
-
 class ModuleImportInfo:
     def __init__(self, import_code, alias) -> None:
         self.import_code = import_code
         self.alias = alias
+
+
+class CodeInfo(ABC):
+    module = ModuleImportInfo
+    torch_name: str
+    params_mapping: dict
+
+    @abstractmethod
+    def generate_code(cls, single_node):
+        pass
--- src/json2torch/json_node/json_node.py
+++ src/json2torch/json_node/json_node.py
@@ -1,6 +1,12 @@
 from abc import ABC, abstractmethod
 
 class JsonNode(ABC):
+    id: str
+    label: str
+    params: dict
+    inputs: dict
+    outputs: dict
+
     @abstractmethod
     def generate_code(self):
         pass
